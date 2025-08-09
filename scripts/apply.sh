64250caa34019f2eec4e0aaef36b1023bb22694b
Bach Nguyen <69bnguyen@gmail.com>
2025-08-09 17:00:25 +0700

    fix terraform scripts

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -7,6 +7,7 @@ source "$(dirname $0)/common.sh"
 terraform_role="terraform-grafana"
 
 vault_login "$terraform_role"
+grafana_auth "grafana/users/admin"
 
 terraform init -backend-config="$(dirname $0)/../envs/${env}/.config/backend.conf" -reconfigure
 terraform apply -var-file="$(dirname $0)/../envs/${env}/.config/terraform.tfvars" "$@"
