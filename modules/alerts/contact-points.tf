6b60756b346dc833c9acf8fa2d5e884f532db22d
Bach Nguyen <69bnguyen@gmail.com>
2026-01-06 00:32:12 +0800

    refactor contact points

--- modules/alerts/contact-points.tf
+++ modules/alerts/contact-points.tf
@@ -1,35 +1,34 @@
 locals {
-  contact-points = merge(
-    [
-      for file_path in fileset(path.module, "../../${var.contact-points-config-dir}/*.yaml") :
-      try(yamldecode(file("${path.module}/${file_path}")), {})
-    ]...
-  )
+  contact-points = {
+    for file_path in fileset(path.module, "../../${var.contact-points-config-dir}/*.yaml") :
+    yamldecode(file("${path.module}/${file_path}")).metadata.name
+    => try(yamldecode(file("${path.module}/${file_path}")), {})
+  }
 }
 
 data "vault_kv_secret_v2" "contact-point-googlechat" {
-  for_each = toset([for k, v in local.contact-points : k if contains(keys(v.contact_points), "googlechat")])
+  for_each = toset([for k, v in local.contact-points : v.metadata.name if contains(keys(v.spec), "googlechat")])
 
   name  = "${var.vault-path-kv-contact-point-googlechat}/${each.value}"
   mount = var.vault-mount-kv
 }
 
 data "vault_kv_secret_v2" "contact-point-slack" {
-  for_each = toset([for k, v in local.contact-points : k if contains(keys(v.contact_points), "slack")])
+  for_each = toset([for k, v in local.contact-points : v.metadata.name if contains(keys(v.spec), "slack")])
 
   name  = "${var.vault-path-kv-contact-point-slack}/${each.value}"
   mount = var.vault-mount-kv
 }
 
 data "vault_kv_secret_v2" "contact-point-telegram" {
-  for_each = toset([for k, v in local.contact-points : k if contains(keys(v.contact_points), "telegram")])
+  for_each = toset([for k, v in local.contact-points : v.metadata.name if contains(keys(v.spec), "telegram")])
 
   name  = "${var.vault-path-kv-contact-point-telegram}/${each.value}"
   mount = var.vault-mount-kv
 }
 
 data "vault_kv_secret_v2" "contact-point-discord" {
-  for_each = toset([for k, v in local.contact-points : k if contains(keys(v.contact_points), "discord")])
+  for_each = toset([for k, v in local.contact-points : v.metadata.name if contains(keys(v.spec), "discord")])
 
   name  = "${var.vault-path-kv-contact-point-discord}/${each.value}"
   mount = var.vault-mount-kv
@@ -39,14 +38,14 @@ resource "grafana_contact_point" "contact-point" {
   for_each = local.contact-points
 
   # requried
-  name = each.value.name
+  name = each.value.metadata.name
 
   disable_provenance = true
   org_id             = var.org-id
 
   # optional
   dynamic "googlechat" {
-    for_each = contains(keys(each.value.contact_points), "googlechat") ? ["googlechat"] : []
+    for_each = contains(keys(each.value.spec), "googlechat") ? ["googlechat"] : []
 
     content {
       # required
@@ -62,7 +61,7 @@ resource "grafana_contact_point" "contact-point" {
 
   # optional
   dynamic "slack" {
-    for_each = contains(keys(each.value.contact_points), "slack") ? ["slack"] : []
+    for_each = contains(keys(each.value.spec), "slack") ? ["slack"] : []
 
     content {
       # required
@@ -87,40 +86,40 @@ resource "grafana_contact_point" "contact-point" {
 
   # optional
   dynamic "telegram" {
-    for_each = contains(keys(each.value.contact_points), "telegram") ? ["telegram"] : []
+    for_each = contains(keys(each.value.spec), "telegram") ? ["telegram"] : []
 
     content {
       # required
-      chat_id = each.value.contact_points["telegram"].chat_id
+      chat_id = each.value.spec["telegram"].chat_id
       token   = jsondecode(data.vault_kv_secret_v2.contact-point-telegram[each.key].data_json).token
 
       # optional
-      disable_notifications    = try(each.value.contact_points["telegram"].disable_notifications, null)
-      disable_resolve_message  = try(each.value.contact_points["telegram"].disable_resolve_message, null)
-      disable_web_page_preview = try(each.value.contact_points["telegram"].disable_web_page_preview, null)
-      message                  = try(each.value.contact_points["telegram"].message, null)
-      message_thread_id        = try(each.value.contact_points["telegram"].message_thread_id, null)
-      parse_mode               = try(each.value.contact_points["telegram"].parse_mode, null)
-      protect_content          = try(each.value.contact_points["telegram"].protect_content, null)
-      settings                 = try(each.value.contact_points["telegram"].settings, null)
+      disable_notifications    = try(each.value.spec["telegram"].disable_notifications, null)
+      disable_resolve_message  = try(each.value.spec["telegram"].disable_resolve_message, null)
+      disable_web_page_preview = try(each.value.spec["telegram"].disable_web_page_preview, null)
+      message                  = try(each.value.spec["telegram"].message, null)
+      message_thread_id        = try(each.value.spec["telegram"].message_thread_id, null)
+      parse_mode               = try(each.value.spec["telegram"].parse_mode, null)
+      protect_content          = try(each.value.spec["telegram"].protect_content, null)
+      settings                 = try(each.value.spec["telegram"].settings, null)
     }
   }
 
   # optional
   dynamic "discord" {
-    for_each = contains(keys(each.value.contact_points), "discord") ? ["discord"] : []
+    for_each = contains(keys(each.value.spec), "discord") ? ["discord"] : []
 
     content {
       # required
       url = jsondecode(data.vault_kv_secret_v2.contact-point-discord[each.key].data_json).url
 
       # optional
-      avatar_url              = try(each.value.contact_points["discord"].avatar_url, null)
-      disable_resolve_message = try(each.value.contact_points["discord"].disable_resolve_message, null)
-      message                 = try(each.value.contact_points["discord"].message, null)
-      settings                = try(each.value.contact_points["discord"].settings, null)
-      title                   = try(each.value.contact_points["discord"].title, null)
-      use_discord_username    = try(each.value.contact_points["discord"].use_discord_username, null)
+      avatar_url              = try(each.value.spec["discord"].avatar_url, null)
+      disable_resolve_message = try(each.value.spec["discord"].disable_resolve_message, null)
+      message                 = file("${path.module}/../../templates/discord.txt")
+      settings                = try(each.value.spec["discord"].settings, null)
+      title                   = file("${path.module}/../../templates/discord-title.txt")
+      use_discord_username    = try(each.value.spec["discord"].use_discord_username, null)
     }
   }
 
